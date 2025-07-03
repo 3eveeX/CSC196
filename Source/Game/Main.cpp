4cb57baf87f7ba8a0d390e6746e6ce8c02bdb47f
3eveeX <hatchjavan@gmail.com>
2025-07-03 10:34:25 -0600

    Added the sdl library among other things and made random lines and dots.

--- Source/Game/Main.cpp
+++ Source/Game/Main.cpp
@@ -1,19 +1,55 @@
+#include <SDL3/SDL.h>
+#include <iostream>
 #include "Math/Math.h"
 #include "Core/Random.h"
-#include <iostream>
 
-#define NAME "Xadia\n"
+    int main(int argc, char* argv[]) {
+        SDL_Init(SDL_INIT_VIDEO);
 
-int main()
-{
-	//float deg = whermst::radToDeg(whermst::pi);
-	whermst::math::min(5, 3);
-	std::cout << NAME;
-	std::cout << "Hello, World!\n";
-	std::cout << whermst::math::pi << std::endl;
-	//std::cout << deg << std::endl;
-	for (int i = 0; i < 10; i++) {
-		std::cout << whermst::random::getRandomFloat() << std::endl;
-	}
+        SDL_Window* window = SDL_CreateWindow("SDL3 Project", 1280, 1024, 0);
+        if (window == nullptr) {
+            std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+            SDL_Quit();
+            return 1;
+        }
 
-}
+        SDL_Renderer* renderer = SDL_CreateRenderer(window, NULL);
+        if (renderer == nullptr) {
+            std::cerr << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
+            SDL_DestroyWindow(window);
+            SDL_Quit();
+            return 1;
+        }
+
+        SDL_Event e;
+        bool quit = false;
+
+
+
+        while (!quit) {
+            while (SDL_PollEvent(&e)) {
+                if (e.type == SDL_EVENT_QUIT) {
+                    quit = true;
+                }
+            }
+
+            SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); // Set render draw color to black
+            SDL_RenderClear(renderer); // Clear the renderer
+
+            for (int i = 0; i < 199; i++) {
+            SDL_SetRenderDrawColor(renderer, whermst::random::getRandomInt(255, 0), whermst::random::getRandomInt(255, 0), whermst::random::getRandomInt(255, 0), 255); // Set random draw color
+            SDL_RenderLine(renderer, whermst::random::getRandomFloat() + whermst::random::getRandomInt(1280, 0), whermst::random::getRandomFloat() + whermst::random::getRandomInt(1024, 0), whermst::random::getRandomFloat() + whermst::random::getRandomInt(1280, 0), whermst::random::getRandomFloat() + whermst::random::getRandomInt(1024, 0));
+            }
+            for (int i = 0; i < 299; i++) {
+                SDL_SetRenderDrawColor(renderer, whermst::random::getRandomInt(255, 0), whermst::random::getRandomInt(255, 0), whermst::random::getRandomInt(255, 0), 255); // Set random draw color
+				SDL_RenderPoint(renderer, whermst::random::getRandomFloat() + whermst::random::getRandomInt(1280, 0), whermst::random::getRandomFloat() + whermst::random::getRandomInt(1024, 0));
+            }
+            SDL_RenderPresent(renderer); // Render the screen
+        }
+
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+
+        return 0;
+    }
